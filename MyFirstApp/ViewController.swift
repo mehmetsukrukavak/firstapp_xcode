9fc091e930c70b6356a773a5e9031b487b3b4d10
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-02 00:39:43 +0300

    Initial commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    
+    let pictures = ["Metallica1", "Metallica2"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    var index = 0
     @IBAction func changeClicked(_ sender: Any) {
-        imageView.image = UIImage(named: "Metallica2")
+      // print(pictures.count)
+        index = Int.random(in: 0..<pictures.count)
+//        if index == 0 {
+//            
+//            index = 1
+//        }else{
+//            index = 0
+//        }
+        imageView.image = UIImage(named: pictures[index])
     }
     
 }
